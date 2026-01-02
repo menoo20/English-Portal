b247ccf305185da2faa9f6e6e284ae0e70e20f58
menoo20 <menooteaching10@gmail.com>
2026-01-02 10:57:36 +0000

    Deploying to gh-pages from @ menoo20/English-Portal@284f807607abd2129c27e3f2ddce93d8a4efc923 🚀

--- _next/static/VMltdM2DdrWARH8H61vpY/_buildManifest.js
+++ _next/static/VMltdM2DdrWARH8H61vpY/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/English-Portal//_next/:path+",
+        "destination": "/English-Portal/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
